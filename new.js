43559926aac62b117a09bd4fadf9ba570c73f0c0
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-01-14 15:59:27 +0530

    first server

---
+++ new.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/Abd' , (req,res)=> {
+    res.json("Whats upp cutieeeeeeeeee")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
