7ed16f602000653b574692833de4f6891517b949
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-01-17 17:10:27 +0530

    todo initialise

---
+++ models/sub_todo.models.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const subTodoschema = new mongoose.Schema(
+    {
+
+    },
+    {timestamps: true}
+)
+
+export const SubTodo = mongoose.model("SubTodo" , subTodoschema) 
