8fbc1126c7c322eb67fda9a98f1e4775dce5c1db
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-01-18 16:45:03 +0530

    user added

---
+++ models/ecommerce/user.models.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";    
+
+const userSchema = new mongoose.Schema(
+    {
+        ussername : {
+            type : String,
+            required : true,
+            unique : true,
+            lowercase : true,
+            trim :true
+        },
+        email : {
+            type : String,
+            required : true,
+            unique : true,
+            lowercase : true,
+            trim :true
+        },
+        password : {
+            type : String,
+            required: true,
+            minlength : 8,
+            trim :true,
+            select : false //prevet from password to leak
+        },
+         role: {
+            type : String,
+            enum: ["user", "admin","seller","moderator"],
+            default: "user",
+        },
+        isActive : {
+            type : Boolean,
+            default : false
+        }
+
+    },{timestamps :true}
+)
+
+export const User = mongoose.model("User" , userSchema)
