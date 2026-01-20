4415ab337809206bc119f973c0d9f0d7ad384544
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-01-20 16:10:20 +0530

    category added

---
+++ models/ecommerce/order.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const customSchema =new mongoose.Schema(
+    {
+        
+    }
+
+)
+
+
+const orderSchema = new mongoose.Schema(
+    {
+    orderPrice: {
+        type: Number,
+        required: true            
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+    
+
+    },
+    {timestamps: true}
+
+)
+export const Order = mongoose.model("Order", orderSchema )
