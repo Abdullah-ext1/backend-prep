18ef9c8dba98b844419622745032d995dd4add9a
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-01-24 16:34:04 +0530

    models shitttt done by meeeeee

--- models/hospital-managment/medical-records.models.js
+++ models/hospital-managment/medical-records.models.js
@@ -1,7 +1,19 @@
 import mongoose from "mongoose";
+import { Patient } from "./patient.models";
 const medicalrecordSchema = mongoose.Schema(
     {
-       
+       name: {
+        type: String,
+        required: true
+       },
+       patientRecord: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient"
+       },
+       docterRecord: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Docter"
+       }
     },{timestamps: true}
 )
 export const MedicalRecord = mongoose.model("MEdicalRecord" , medicalrecordSchema)
